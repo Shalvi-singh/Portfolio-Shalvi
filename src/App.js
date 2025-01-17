27647d92c3f6e4e3e6d8f9c63da3d19ced945272
Shalvi-singh <shalvisingh044@gmail.com>
2025-01-17 18:38:53 +0530

    checking update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
    <div className='content'>
    <h1 className='h1'> Small Intro!</h1> 
     <p>Hey! I appreciate that you are here to know about an undergrad girl, <strong>Shalvi Singh</strong>, and that is me. <p>Hii,I am currently pursuing Bachelors of vocation in (web designing) from kalindi college, University of Delhi.</p>
-     <p> My coding journey as a newbie web developer begans with the humble html and css,and the rest is history. </p>
+     <p> My coding journey as a  web developer begans with the humble html and css,and the rest is history. </p>
      <p>When I'm not coding, you may find me exploring the great outdoors, experimenting with new colours, or diving into the beauty of Earth.
      </p></p>
   </div>
